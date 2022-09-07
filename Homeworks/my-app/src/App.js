f084355de497b9d669160a55f7a4b4775e4c7bc2
Ivan-Dell <vanbog@mail.ru>
2022-09-08 00:52:03 +0300

    lesson 3 - update after teacher comments

--- Homeworks/my-app/src/App.js
+++ Homeworks/my-app/src/App.js
@@ -8,15 +8,16 @@ function App() {
 
   const [text, setText] = useState('');
   const [author, setAuthor] = useState('');
-  const [state, setState] = useState([{ text: '', author: 'user' }]);
+  const [messageList, setState] = useState([{ text: '', author: '' }]);
 
   const handleChange = (event) => {
-    setState(event.target.value)
+    setText(event.target.value);
+    setAuthor('user');
   }
 
-  function submitHandle(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    if (state.length > 0) {
+    if (messageList.length > 0) {
       setState(pervstate => [...pervstate, { text, author }]);
     }
   }
@@ -27,24 +28,29 @@ function App() {
     inputRef.current?.focus();
   }, []);
 
+
   return (
-    <React.Fragment>
+    <>
       <TextField
         style={{ margin: '20px' }}
         id="outlined-basic"
         label="Outlined"
         variant="outlined"
-        value={state}
+        value={text}
         onChange={handleChange}
       />
-      <form onSubmit={submitHandle}>
+      <form onSubmit={handleSubmit}>
         Your Message:
         <input ref={inputRef} type="text" onChange={handleChange} />
         <input type="submit" value="Send" />
       </form>
-      {state.map((message, index) => <div key={index}>Author {message.author} his mess: {message.text}</div>)}
-      {chats.map((chat) => <div key={chat.id}>{chat.name}</div>)}
-    </React.Fragment>
+      <table>
+        <tr>
+          <td valign='top' align='left'>{chats.map((chat) => <div key={chat.id}><h2>{chat.name}:</h2></div>)}</td>
+          <td valign='top' align='right'>{messageList.map((message, index) => <div key={index}><h3>{message.author}</h3> <p>{message.text}</p></div>)}</td>
+        </tr>
+      </table>
+    </>
   );
 }
 
